6fbb70d4fe57a83b035996d25169a37c4a441635
Ian Dass <iandass@Ians-MacBook-Pro.local>
2016-04-13 19:17:34 -0400

    latest

--- twitterApp.js
+++ twitterApp.js
@@ -5,7 +5,7 @@ var http = require('http');
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 var bodyParser = require('body-parser');
-var search, cnt, count, date;
+var search, cnt, count, date, post;
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -16,7 +16,6 @@ var T = new Twit({
   , access_token_secret:  '7W7aPU3NZwtjQ7NpESibOEYyITx6o9ItYiIK0pTOAzR60' // Your Access Token Secret
 });
 
-search = "Ryerson";
 
 server.listen(8081);
 
@@ -32,44 +31,108 @@ app.get('/', function(req,res) {
 
 
 
-app.post('/process_post', urlencodedParser, function (req, res) {
 
-   // Prepare output in JSON format
-   response = {
-       keyword:req.body.keyword,
-       counts:req.body.counts
-   };
-   console.log(response);
 
-   search = JSON.stringify(response.keyword);
-   cnt =  JSON.parse(response.counts);
-   
-   res.redirect('http://127.0.0.1:8081');
-   
-   console.log(count);
-   //res.end(JSON.stringify(response));
-});
+//var buttonValue = req.body.go;
+
 
 
 io.sockets.on('connection', function (socket) {
   console.log('Connected');
   
   
-    T.get('search/tweets', { q: search, count: cnt }, function(err, data, response) {
-    
-        for (var i = 0; i < cnt; i++){
-            if (data.statuses[i] == undefined) {
-                
-            console.log("undefined tweet error")
-            
-            }else{
-                
-            io.sockets.emit('created',JSON.stringify(data.statuses[i].created_at));
-            io.sockets.emit('text',JSON.stringify(data.statuses[i].text));
-            io.sockets.emit('user',JSON.stringify(data.statuses[i].user.screen_name));
-            }
-        } 
+  
+  //if (buttonValue == "Submit") {
+    app.post('/process_post', urlencodedParser, function (req, res) {
+        
+        var buttonValue = req.body.go;
+        console.log(buttonValue);
 
+    if (buttonValue == "Submit") {
+            
+        
+         // Prepare output in JSON format
+        response = {
+            keyword:req.body.keyword,
+            counts:req.body.counts
+        };
+        //console.log(response);
+
+        if ( response.keyword == "" ) {
+             console.log("enter search keyword");
+        
+         }else if (response.counts == "") {
+             console.log("enter number of tweets to return");
+         }else{
+             search = JSON.stringify(response.keyword);
+             cnt =  JSON.parse(response.counts);
+         }
+   
+        res.redirect('http://127.0.0.1:8081');
+   
+        //console.log(count);
+        //res.end(JSON.stringify(response));
+     
+        T.get('search/tweets', { q: search, count: cnt }, function(err, data, response) {
+            for (var i = 0; i < cnt; i++){
+                if (data.statuses[i] == undefined) {
+                
+                console.log("undefined tweet error");
+            
+                }else{
+                
+                io.sockets.emit('created',JSON.stringify(data.statuses[i].created_at));
+                io.sockets.emit('text',JSON.stringify(data.statuses[i].text));
+                io.sockets.emit('user',JSON.stringify(data.statuses[i].user.screen_name));
+                }
+            } 
+
+        });
+    }else if (buttonValue == "Post") {
+        
+        response = {
+            postTweet:req.body.postTweet
+        };
+
+        post = JSON.stringify(response.postTweet);
+        console.log(post);
+
+        res.redirect('http://127.0.0.1:8081');
+   
+   
+        T.post('statuses/update', { status: post }, function(err, data, response) {
+            console.log(data);
+            io.sockets.emit('you',JSON.stringify(data.user.screen_name));
+            io.sockets.emit('posted',JSON.stringify(data.text));
+        });
+    
+    }
+    
+    
     });
     
- });
+    
+  /*}else if (buttonValue == "Post") {
+     
+    app.post('/process_post2', urlencodedParser, function (req, res) {
+
+   // Prepare output in JSON format
+        response = {
+            postTweet:req.body.postTweet
+        };
+
+        post = JSON.stringify(response.postTweet);
+        console.log(post);
+
+        res.redirect('http://127.0.0.1:8081');
+   
+   //res.end(JSON.stringify(response));
+   
+        T.post('statuses/update', { status: post }, function(err, data, response) {
+            console.log(data);
+        });
+    });
+  }*/
+    
+    
+});
